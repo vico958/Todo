9c95cad727dcc2bee1eef3f50f84443a274671c4
vico958 <vico958@gmail.com>
2024-03-24 00:56:43 +0200

    removed in app loaderAction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,25 +5,17 @@ import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
 import useTodoStore from './zustand/todo/store';
 import { Flex, Text } from '@chakra-ui/react';
 import Loader from './components/loader/Loader';
-import LoaderAfterAction from "./components/loader/LoaderAfterAction";
 const App = () => {
   const { todos, setTodosOnStartOfApp } = useTodoStore();
   const user = useAuthUser();
-  const [showLoaderAfterChange, setShowLoaderAfterChange] = useState(false);
+  
   useEffect(() =>{
     const gettingData = async() => {
       const data = await todoClient.getAllTodoOfUser(user.token);
       setTodosOnStartOfApp(data)
     }
-    try{
-      setShowLoaderAfterChange(true);
       gettingData();
-    }catch(error){
-      console.log(error.message);
-    }finally{
-      setShowLoaderAfterChange(false);
-    }
-  },[todos])
+  },[])
 
   if(todos === null){
     return <Loader/>
@@ -37,11 +29,8 @@ const App = () => {
     )
   }
 
-  return (<>
-  {showLoaderAfterChange? <LoaderAfterAction/> :
+  return (
     <TodoContainer/>
-  }
-  </>
   )
 }
 
